e15cbb30dba6b6fa721986efcee4e597556d7836
Petre Vane <petre.vane@gmail.com>
2020-10-21 14:10:18 +0200

    Initial Commit

---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by Petre Vane on 21/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
